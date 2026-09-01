6d1ae8660322173ab5277cc9dc809487b91eb3f0
Ariela Rivalta <ariela.rivalta.ts@gmail.com>
2026-09-01 16:38:49 -0300

    feat: agregar validaciones y pruebas para la función obtener_calificacion

--- Actividad_clase_3/Modulo_1/obtener_calificacion.py
+++ Actividad_clase_3/Modulo_1/obtener_calificacion.py
@@ -13,6 +13,7 @@
 # print(obtener_calificacion(8))
 # print(obtener_calificacion(6))
 # print(obtener_calificacion(4))
+#validar que sea del 1 al 10 y qeu no sea letra, que no sea una lista
 
 # Codigo corregido
 def obtener_calificacion(nota):
@@ -33,4 +34,40 @@ def test_obtener_calificacion():
       obtener_calificacion(11)
       assert False
   except ValueError:
+    print("Error")
+    assert True
+      
+def test_nota_es_string():
+  try:
+        obtener_calificacion("diez")
+        assert False, "Debería haber lanzado TypeError por string"
+  except TypeError:
+    print("Error")
+    assert True
+
+# Test 2: Lista lanza TypeError
+def test_nota_es_lista():
+    try:
+        obtener_calificacion([10])
+        assert False, "Debería haber lanzado TypeError por lista"
+    except TypeError:
+      print("Error")
+      assert True
+
+# Test 3: Booleano lanza TypeError
+def test_nota_es_booleano():
+    try:
+        obtener_calificacion(True)
+        assert False, "Debería haber lanzado TypeError por booleano"
+    except TypeError:
+      print("Error")
+      assert True
+
+# Test 4: Número fuera de rango lanza ValueError
+def test_nota_fuera_de_rango():
+    try:
+        obtener_calificacion(11)
+        assert False, "Debería haber lanzado ValueError por número mayor a 10"
+    except ValueError:
+      print("Error")
       assert True
