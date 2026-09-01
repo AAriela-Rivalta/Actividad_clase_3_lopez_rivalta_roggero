92fc9906c5676e1fac49f24943d3ddd9dc02c84f
Ariela Rivalta <ariela.rivalta.ts@gmail.com>
2026-09-01 17:15:28 -0300

    feat: corregir función buscar_producto para devolver el producto en lugar del índice

--- Actividad_clase_3/Modulo_3/buscar_producto.py
+++ Actividad_clase_3/Modulo_3/buscar_producto.py
@@ -4,13 +4,21 @@
 #         if prod["nombre"] == nombre:
 #             return i
 #     return None
-  
+
+
 # Codigo corregido
 def buscar_producto(inventario, nombre):
     for prod in inventario:
         if prod["nombre"] == nombre:
             return prod
     return None
+  
+print(buscar_producto(
+  [
+  {"nombre": "Teclado", "stock": 4},
+  {"nombre": "Mouse", "stock": 3}
+  ], "Teclado"
+  ))  # Devuelve el diccionario del producto
   
 # Prueba unutaria
 def test_buscar_producto():
